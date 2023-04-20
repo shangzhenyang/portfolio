382c62220eab0de085a631d71ae2ba9c9da52f3a
Shangzhen Yang <shangzheny01@gmail.com>
2023-04-20 00:57:36 -0700

    Make it static

--- next.config.js
+++ next.config.js
@@ -1,41 +1,10 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "assets.retiehe.com"
-			}
-		]
+		unoptimized: true
 	},
-	reactStrictMode: true,
-	async headers() {
-		if (process.env.NODE_ENV === "development") {
-			return [];
-		}
-		return [
-			{
-				source: "/",
-				headers: [{
-					"key": "Content-Security-Policy",
-					"value": "default-src 'none'; base-uri 'none'; connect-src https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com; form-action 'none'; frame-ancestors 'none'; img-src 'self' https://assets.retiehe.com https://developer.apple.com https://*.google-analytics.com https://*.googletagmanager.com; script-src 'self' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'"
-				}, {
-					"key": "Referrer-Policy",
-					"value": "strict-origin-when-cross-origin"
-				}, {
-					"key": "Strict-Transport-Security",
-					"value": "max-age=31536000"
-				}]
-			}
-		];
-	},
-	async redirects() {
-		return [{
-			"source": "/favicon.ico",
-			"destination": "https://assets.retiehe.com/ysz/avatar.png",
-			"permanent": true
-		}];
-	}
+	output: "export",
+	reactStrictMode: true
 };
 
 module.exports = nextConfig;
