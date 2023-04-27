097c80d70a8d83bb8ca4c6654bd0999a0df85dd0
Shangzhen Yang <shangzheny01@gmail.com>
2023-04-27 10:08:09 -0700

    Minor changes

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -16,107 +16,134 @@ interface Group {
 }
 
 function LinkList() {
-	const groups: Group[] = [{
-		title: t("mainProjects"),
-		items: [{
-			title: t("airportal"),
-			link: "https://airportal.cn/",
-			description: t("airportalDescription"),
-			extra: isChinaSite ? undefined : (
-				<DownloadBar
-					appName="AirPortal"
-					appStoreLink="https://apps.apple.com/app/id1524074327"
-					googlePlayLink="https://play.google.com/store/apps/details?id=cn.airportal&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
-				/>
-			),
+	const groups: Group[] = [
+		{
+			title: t("mainProjects"),
+			items: [
+				{
+					title: t("airportal"),
+					link: "https://airportal.cn/",
+					description: t("airportalDescription"),
+					extra: isChinaSite ? undefined : (
+						<DownloadBar
+							appName="AirPortal"
+							appStoreLink="https://apps.apple.com/app/id1524074327"
+							googlePlayLink="https://play.google.com/store/apps/details?id=cn.airportal&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+						/>
+					),
+					when: true
+				},
+				{
+					title: t("limeStartPage"),
+					link: "https://limestart.cn/intro",
+					description: t("limeStartPageDescription"),
+					when: true
+				},
+				{
+					title: t("toolbox"),
+					link: "https://tool.retiehe.com/",
+					description: t("toolboxDescription"),
+					when: true
+				},
+				{
+					title: t("webHosting"),
+					link: "https://host.retiehe.com/",
+					description: t("webHostingDescription"),
+					when: true
+				}
+			],
 			when: true
-		}, {
-			title: t("limeStartPage"),
-			link: "https://limestart.cn/intro",
-			description: t("limeStartPageDescription"),
-			when: true
-		}, {
-			title: t("toolbox"),
-			link: "https://tool.retiehe.com/",
-			description: t("toolboxDescription"),
-			when: true
-		}, {
-			title: t("webHosting"),
-			link: "https://host.retiehe.com/",
-			description: t("webHostingDescription"),
-			when: true
-		}],
-		when: true
-	}, {
-		title: "Collaborative Project",
-		items: [{
-			title: "Hua Classmate",
-			link: "https://uwclassmate.com/",
-			description: "A course commenting website for UW students.",
-			when: true
-		}],
-		when: !isChinaSite
-	}, {
-		title: t("openSourcedProjects"),
-		items: [{
-			title: t("encoder"),
-			link: "https://encoder.shangzhenyang.com/",
-			description: t("encoderDescription"),
-			when: true
-		}, {
-			title: t("randomNumber"),
-			link: "https://random.shangzhenyang.com/",
-			description: t("randomNumberDescription"),
-			when: true
-		}, {
-			title: t("bingWallpaperApi"),
-			link: "https://github.com/shangzhenyang/bing-wallpaper",
-			description: t("bingWallpaperApiDescription"),
-			when: true
-		}, {
-			title: t("periodicTable"),
-			link: "https://github.com/shangzhenyang/periodic-table",
-			description: t("periodicTableDescription"),
-			when: true
-		}],
-		when: true
-	}, {
-		title: t("toolbox"),
-		items: [{
-			title: t("wordList"),
-			link: "https://tool.retiehe.com/wordlist",
-			when: true
-		}, {
-			title: t("timer"),
-			link: "https://tool.retiehe.com/timer",
-			when: true
-		}, {
-			title: t("calculator"),
-			link: "https://tool.retiehe.com/calculator",
-			when: true
-		}, {
-			title: t("marquee"),
-			link: "https://tool.retiehe.com/marquee",
-			when: true
-		}, {
-			title: t("textEditor"),
-			link: "https://tool.retiehe.com/texteditor",
-			when: true
-		}],
-		when: true
-	}, {
-		title: t("otherProjects"),
-		items: [{
-			title: "Unofficial ChatGPT Client",
-			link: "https://chatgpt.shangzhenyang.com/",
+		},
+		{
+			title: "Collaborative Project",
+			items: [
+				{
+					title: "Hua Classmate",
+					link: "https://uwclassmate.com/",
+					description: "A course commenting website for UW students.",
+					when: true
+				}
+			],
 			when: !isChinaSite
-		}, {
-			title: t("uwCampusPaths"),
-			link: "https://paths.shangzhenyang.com/",
+		},
+		{
+			title: t("openSourcedProjects"),
+			items: [
+				{
+					title: t("encoder"),
+					link: "https://encoder.shangzhenyang.com/",
+					description: t("encoderDescription"),
+					when: true
+				},
+				{
+					title: t("randomNumber"),
+					link: "https://random.shangzhenyang.com/",
+					description: t("randomNumberDescription"),
+					when: true
+				},
+				{
+					title: t("bingWallpaperApi"),
+					link: "https://github.com/shangzhenyang/bing-wallpaper",
+					description: t("bingWallpaperApiDescription"),
+					when: true
+				},
+				{
+					title: t("periodicTable"),
+					link: "https://github.com/shangzhenyang/periodic-table",
+					description: t("periodicTableDescription"),
+					when: true
+				}
+			],
 			when: true
-		}],
-		when: true
-	}];
+		},
+		{
+			title: t("toolbox"),
+			items: [
+				{
+					title: t("wordList"),
+					link: "https://tool.retiehe.com/wordlist",
+					when: true
+				},
+				{
+					title: t("timer"),
+					link: "https://tool.retiehe.com/timer",
+					when: true
+				},
+				{
+					title: t("calculator"),
+					link: "https://tool.retiehe.com/calculator",
+					when: true
+				},
+				{
+					title: t("marquee"),
+					link: "https://tool.retiehe.com/marquee",
+					when: true
+				},
+				{
+					title: t("textEditor"),
+					link: "https://tool.retiehe.com/texteditor",
+					when: true
+				}
+			],
+			when: true
+		},
+		{
+			title: t("otherProjects"),
+			items: [
+				{
+					title: "Unofficial ChatGPT Client",
+					link: "https://chatgpt.shangzhenyang.com/",
+					when: !isChinaSite
+				},
+				{
+					title: t("uwCampusPaths"),
+					link: "https://paths.shangzhenyang.com/",
+					when: true
+				}
+			],
+			when: true
+		}
+	];
 
 	const groupElements = groups.map((group) => {
 		if (!group.when) {
@@ -132,16 +159,20 @@ function LinkList() {
 		);
 	});
 
-	const friends = [{
-		title: "毛若昕",
-		link: "https://maorx.cn/",
-	}, {
-		title: "空白的羽翼",
-		link: "https://blankwings.com/"
-	}, {
-		title: "Idealclover",
-		link: "https://idealclover.top/"
-	}];
+	const friends = [
+		{
+			title: "毛若昕",
+			link: "https://maorx.cn/",
+		},
+		{
+			title: "空白的羽翼",
+			link: "https://blankwings.com/"
+		},
+		{
+			title: "Idealclover",
+			link: "https://idealclover.top/"
+		}
+	];
 
 	const friendElements = friends.map(({ link, title }) => {
 		return (
--- src/i18n.ts
+++ src/i18n.ts
@@ -4,11 +4,14 @@ import translationZhCn from "@/translations/zh-cn.json";
 const isChinaSite = process.env.NEXT_PUBLIC_LOCALE === "cn";
 
 function t(key: string): string {
+	let result: string | undefined = undefined;
 	if (isChinaSite) {
-		return translationZhCn[key as keyof typeof translationZhCn];
-	} else {
-		return translationEnUs[key as keyof typeof translationEnUs];
+		result = translationZhCn[key as keyof typeof translationZhCn];
 	}
+	if (!result) {
+		result = translationEnUs[key as keyof typeof translationEnUs];
+	}
+	return result || key;
 }
 
 export { isChinaSite, t };
