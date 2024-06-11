1bc403ba63437b2151ce8db79165e0d84cf53bbf
Shangzhen Yang <shangzheny01@gmail.com>
2024-06-11 01:35:31 -0700

    Update OpenToWorkBadge.tsx

--- src/components/OpenToWorkBadge.tsx
+++ src/components/OpenToWorkBadge.tsx
@@ -1,6 +1,11 @@
 function OpenToWorkBadge(): JSX.Element {
 	return (
-		<div className="bg-green-800 text-white shadow font-bold w-fit -mt-2.5 mx-auto px-2.5 py-1 rounded-xl">#OpenToWork</div>
+		<div
+			className="relative bg-green-800 text-white shadow font-bold w-fit -mt-2.5 mx-auto px-2.5 py-1 rounded-xl"
+			title="Seeking software engineering opportunities."
+		>
+			#OpenToWork
+		</div>
 	);
 }
 
